f80cb44da7238d981817f2bddbc93a39ce682259
Dayloc Yumar <daylocyumar@gmail.com>
2025-02-23 10:03:10 +0100

    Base de boilerplate

--- src/js/components/demo.jsx
+++ src/js/components/demo.jsx
@@ -5,16 +5,12 @@ import "./../../styles/demo.css";
 
 const Demo = () => {
   const { store, actions } = useFlux();
-  const { dataCharacters } = store;
-  const { dataPlanets } = store;
-
+ 
   useEffect(() => {
-    actions.getCharacters();
-    actions.getPlanets();
+   
   }, []);
 
-  console.log(dataCharacters);
-  console.log(dataPlanets);
+ 
 
   return (
     <div className="container p-3 bg-dark text-warning ">
@@ -22,34 +18,7 @@ const Demo = () => {
       <div>
         <Link to="/">Home</Link>
       </div>
-      <div className="card-container">
-        {dataCharacters &&
-          dataCharacters.items && // Verifica que `items` exista
-          dataCharacters.items.map((item, index) => (
-            <div key={index} className="card">
-              <img src={item.image} className="card-img-top" alt={item.name} />
-              <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
-                <p className="card-text">{item.description}</p>
-              </div>
-            </div>
-          ))}
-      </div>
-      <h1 className="m-3">Planetas:</h1>
-
-      <div className="card-container mt-3">
-        {dataPlanets &&
-          dataPlanets.items && // Verifica que `items` exista
-          dataPlanets.items.map((item, index) => (
-            <div key={index} className="card">
-              <img src={item.image} className="card-img-top" alt={item.name} />
-              <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
-                <p className="card-text">{item.description}</p>
-              </div>
-            </div>
-          ))}
-      </div>
+     
     </div>
   );
 };
--- src/js/flux/flux.jsx
+++ src/js/flux/flux.jsx
@@ -2,27 +2,11 @@ import { useState } from "react";
 
 const useFlux = () => {
   const [store, setStore] = useState({
-    user: null,
-    isAuthenticated: false,
-    dataCharacters: null, // Inicialmente null
-    dataPlanets: null, // Inicialmente null
+   dataPlanets: null, // Inicialmente null
   });
 
   const actions = {
-    login: (userData) => {
-      setStore((prevStore) => ({
-        ...prevStore,
-        user: userData,
-        isAuthenticated: true,
-      }));
-    },
-    logout: () => {
-      setStore((prevStore) => ({
-        ...prevStore,
-        user: null,
-        isAuthenticated: false,
-      }));
-    },
+   
     getCharacters: async () => {
       try {
         const response = await fetch(
@@ -40,23 +24,7 @@ const useFlux = () => {
         console.error("Error fetching characters:", error);
       }
     },
-    getPlanets: async () => {
-      try {
-        const response = await fetch(
-          "https://dragonball-api.com/api/planets?limit=10",
-        );
-        if (!response.ok) {
-          throw new Error("Error al obtener los datos de planetas");
-        }
-        const data = await response.json();
-        setStore((prevStore) => ({
-          ...prevStore,
-          dataPlanets: data, // Actualiza el estado con los datos
-        }));
-      } catch (error) {
-        console.error("Error fetching planets:", error);
-      }
-    },
+   
   };
 
   return { store, actions };
